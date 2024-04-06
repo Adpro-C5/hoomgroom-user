ba1dbf7890d33e422085428a9af7a4952a00e24a
hilaldfzn <hilalfauzan9@gmail.com>
2024-04-06 21:11:16 +0700

    first commit

---
+++ src/test/java/id/ac/ui/cs/advprog/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
